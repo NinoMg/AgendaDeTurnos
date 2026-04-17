a07a05ba236eeab61a86f0fed7b63fc0e51622a8
nino <nino@debianserver>
2026-04-17 09:30:18 -0300

    agrego telefono seguro

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "dev")
 
 # Inicializar base de datos
+def agregar_columna_si_no_existe():
+    conn = sqlite3.connect('turnos.db')
+    c = conn.cursor()
+
+    try:
+        c.execute("ALTER TABLE turnos ADD COLUMN telefono TEXT")
+    except:
+        pass  # si ya existe, no hace nada
+
+    conn.commit()
+    conn.close()
+    
 def init_db():
     conn = sqlite3.connect('turnos.db')
     c = conn.cursor()
@@ -35,12 +47,17 @@ def agregar():
     nombre = request.form.get('nombre', '').strip()
     fecha = request.form.get('fecha', '').strip()
     hora = request.form.get('hora', '').strip()
-
+    telefono = request.form.get('telefono', '').strip()
+    
     # Validación
     if not nombre or not fecha or not hora:
         flash("Complete todos los campos", 'warning')
         return redirect('/')
 
+    if not nombre or not fecha or not hora or not telefono:
+        flash("Complete todos los campos", 'warning')
+        return redirect('/')
+        
     conn = sqlite3.connect('turnos.db')
     c = conn.cursor()
 
@@ -55,8 +72,8 @@ def agregar():
 
     # Insertar turno
     c.execute(
-        "INSERT INTO turnos (nombre, fecha, hora) VALUES (?, ?, ?)",
-        (nombre, fecha, hora)
+        "INSERT INTO turnos (nombre, fecha, hora, telefono) VALUES (?, ?, ?, ?)",
+        (nombre, fecha, hora, telefono)
     )
     conn.commit()
     conn.close()
@@ -76,5 +93,6 @@ def eliminar(id):
     return redirect('/')
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+        init_db()
+        agregar_columna_si_no_existe()
+        app.run(debug=True)
